b71cb736a33da8baef97370c1ec0d391ddb2881f
Katreque <renanverissimo@uol.com.br>
2017-03-23 11:31:16 -0300

    Angular + bootstrap added on popup.

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,15 +1,15 @@
+'use strict';
+
 (function () {
   'use strict';
-
   /*chrome.storage.local.remove('value');
-
-    chrome.storage.local.set({ 'value': 'Fon!' }, function () {
+     chrome.storage.local.set({ 'value': 'Fon!' }, function () {
     console.log('Oh Right!');
-
-     localStorage.setItem('kappa', 'Churrasqueira Controle Remoto');
+      localStorage.setItem('kappa', 'Churrasqueira Controle Remoto');
   });*/
 
   //Por localStorage
+
   console.log(localStorage.getItem('kappa'));
 
   //Por API
@@ -17,15 +17,19 @@
     console.log(res);
   });
 
-  var kappa = function kappa() {
-    console.log('Seu Kappa!');
-  };
+  angular.module('app').controller('mainController', function () {
+    var self = this;
 
-  var pride = function pride() {
-    console.log('Seu KappaPride!');
-  };
+    this.kappa = function () {
+      console.log('Seu Kappa!');
+    };
 
-  var fon = function fon() {
-    console.log('Fon!');
-  };
+    this.pride = function () {
+      console.log('Seu KappaPride!');
+    };
+
+    this.fon = function () {
+      console.log('Fon!');
+    };
+  });
 })();
