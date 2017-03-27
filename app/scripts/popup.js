eba6c60bb6771091dd89177a176c6b604b6e7a33
Katreque <renanverissimo@uol.com.br>
2017-03-27 17:51:54 -0300

    Ajuste no manifest para que o popup voltasse a abrir normalmente.

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -5,20 +5,21 @@
 
   angular.module('app').controller('mainController', function () {
     var self = this;
-    /*chrome.storage.local.remove('value');
-       chrome.storage.local.set({ 'value': 'Fon!' }, function () {
+
+    /*
+    chrome.storage.local.remove('value');
+    chrome.storage.local.set({ 'value': 'Fon!' }, function () {
       console.log('Oh Right!');
-        localStorage.setItem('kappa', 'Churrasqueira Controle Remoto');
+      localStorage.setItem('kappa', 'Churrasqueira Controle Remoto');
     });*/
 
     //Por localStorage
-
     console.log(localStorage.getItem('kappa'));
 
-    //Por API
-    chrome.storage.local.get('value', function (res) {
+    /*Por API
+      chrome.storage.local.get('value', function (res) {
       console.log(res);
-    });
+    });*/
 
     this.kappa = function () {
       console.log('Seu Kappa!');
