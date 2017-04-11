9bdd0760d2d7bb088ada97b61f6500cac38f8655
Katreque <renanverissimo@uol.com.br>
2017-04-11 10:01:00 -0300

    Minor fixes + adição de logs para depuração.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,25 +3,31 @@
 (function () {
   'use strict';
 
-  //console.log('background está funcionando está up!');
+  //console.log('Background está funcionando!');
+  //Listener da contentscript
 
   chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     return new Promise(function (res, rej) {
+      //console.log(msg);
+
       var port = chrome.runtime.connectNative('kappa.native');
-      //console.log('Mandando a churrasqueira controle remoto!');
+      //console.log('Porta de comunicação aberta');
 
       port.postMessage({ 'text': msg.message });
+      //console.log('Mensagem enviada');
 
+      //Retorno da aplicação nativa
       return new Promise(function (res, rej) {
         port.onMessage.addListener(function (msg) {
           chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            //console.log(msg);
             chrome.tabs.sendMessage(tabs[0].id, msg);
             //console.log('Mandando de volta para a content!');
           });
         });
 
         port.onDisconnect.addListener(function () {
-          //console.log('Desconectado, ao VIVASSO!');
+          //console.log('Fechando porta de comunicação');
         });
       });
     });
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -9,25 +9,24 @@
     }
   };
 
-  var callback = function callback() {
-    var k = new CustomEvent('ExtensaoParaPage', objeto);
-    window.dispatchEvent(k);
-  };
-
-  var callbackk1 = function callbackk1(msg) {
-    chrome.runtime.sendMessage(msg);
-  };
-
-  chrome.runtime.onMessage.addListener(function (evt) {
-    objeto.detail.message = evt.text;
-    return callback();
-  }), false;
-
+  //Listener da page
   window.addEventListener('PageParaExtensao', function (evt) {
     return new Promise(function (res, rej) {
-      return callbackk1(evt.detail);
+      //Envia mensagem para a background com o objeto recebido da página;
+      chrome.runtime.sendMessage(evt);
+      //console.log(evt);
     });
   }, false);
 
+  //Remove listener depois que foi ouvido.
   window.removeEventListener('PageParaExtensao', function (evt) {});
+
+  //Listener do background ao fim do processamento
+  chrome.runtime.onMessage.addListener(function (evt) {
+    //console.log(evt);
+    objeto.detail.message = evt.text;
+    //Envia o objeto tratado para a page novamente
+    var k = new CustomEvent('ExtensaoParaPage', objeto);
+    window.dispatchEvent(k);
+  }), false;
 })();
